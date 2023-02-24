67302abcc7305301b8ca94613e8b5cb24c3442ec
Natan-asrat <nathanyilmaasrat@gmail.com>
2023-02-24 14:00:42 -0500

    organize list

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let notificationList=[
-    {
-        from: '',
-        message: '',
-        time: '',
-        key: ''
-    }
-];
-
 var main=document.querySelector("#notifications");
 
 function update () {
@@ -35,3 +26,48 @@ function update () {
 </div>
 */
 document.querySelector("body").onload=update;
+
+let notificationList=[
+    {
+        from: 'Mark Webber',
+        message: 'reacted to your recent post My first tournament today!',
+        time: '1m ago',
+        key: '1'
+    },
+    {
+        from: 'Angela Gray',
+        message: 'followed you',
+        time: '5m ago',
+        key: '2'
+    },
+    {
+        from: 'Jacob Thompson',
+        message: 'has joined your group Chess Club',
+        time: '1 day ago',
+        key: '3'
+    },
+    {
+        from: 'Rizky Hasanuddin',
+        message: 'sent you a private message',
+        time: '5 days ago',
+        key: '4'
+    },
+    {
+        from: 'Kimberly Smith',
+        message: 'commented on your picture',
+        time: '1 week ago',
+        key: '5'
+    },
+    {
+        from: 'Nathan Peterson',
+        message: 'reacted to your recent post 5 end-game strategies to increase your win rate',
+        time: '2 weeks ago',
+        key: '6'
+    },
+    {
+        from: 'Anna Kim',
+        message: 'left the group Chess Club',
+        time: '2 weeks ago',
+        key: '7'
+    }
+];
